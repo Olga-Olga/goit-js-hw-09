ee1eacf179e6f3ee6700d713967deb3cf0639c79
Olga-Olga <olga_l@ukr.net>
2023-07-06 08:49:58 +0300

    7654654

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,25 +15,59 @@ function callback(event) {
   step = Number(event.target.elements.step.value)
   amount = Number(event.target.elements.amount.value);
   Notify.warning(`Get valuve: ${delay}, ${step}, ${amount}`)
-  setTimeout(() => {
 
-  for (let i = 0; i < amount; i++) {    
-      createPromise(i, step)
-        .then(({ position, delay }) => {
-          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch(({ position, delay }) => {
-          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
+    const promises = [];
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const promise = createPromise(position, delay);
+    promises.push(promise);
   }
-    
-  }, delay)
+
+  let counter = 0;
+
+  const intervalID = setInterval(() => {
+    if (counter >= promises.length) {
+      clearInterval(intervalID);
+      return;
+    }
+
+    const promise = promises[counter];
+    promise
+      .then(({ position, delay }) => {
+        console.log(`✅ Промис ${position} выполнен через ${delay} мс`);
+        Notify.success(`✅ Промис ${position} выполнен через ${delay} мс`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Промис ${position} отклонен через ${delay} мс`);
+        Notify.failure(`❌ Промис ${position} отклонен через ${delay} мс`);
+      });
+
+    counter++;
+  }, step);
 }
 
+  // const setTimeoutID = setTimeout(() => {
+//     setInterval (() => {
+//     for (let i = 0; i < amount; i++) {
+//       createPromise(i, step)
+//         .then(({ position, delay }) => {
+//           console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//           Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//         })
+//         .catch(({ position, delay }) => {
+//           console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//           Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//         });
+//     }
+//   // clearTimeout(setTimeoutID)
+//   //   }, step) 
+  
+//   }, delay)
+// }
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;  
+  const shouldResolve = Math.random() > 0.5;  
   const myPromise = new Promise((res, rej) => {
     if (shouldResolve) {
       res({ position, delay });
